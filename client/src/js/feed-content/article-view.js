788c1ddf0e8eca5b1209d77a12577b9b38697971
Jeremiah Orr <jerry.orr@spiderstrategies.com>
2014-03-20 13:39:19 -0400

    show full article on click

--- client/src/js/feed-content/article-view.js
+++ client/src/js/feed-content/article-view.js
@@ -1,17 +1,60 @@
 var Backbone = require('backbone')
   , _ = require('underscore')
+  , $ = require('jQuery')
 
 module.exports = Backbone.View.extend({
   className: 'fr-feed-content-article',
 
+  events: {
+    'click': 'maybeShowFullContent'
+  },
+
   render: function () {
     this.$el.append('<span class="title">' + this.model.get('title') + '</span>'
-      + '<span class="summary">' + this.model.get('summary') + '</span>'
       + '<a class="link btn btn-default" target="fr-' + this.model.id + '" href="' + this.model.get('link') + '" title="View Full Page">'
       + '<span class="glyphicon glyphicon-arrow-right"></span></a>')
 
-    // TODO logic to show full article ('description')
+
+    this.toggleFullContent(false)
 
     return this
+  },
+
+  maybeShowFullContent: function (e) {
+    if (this.$el.find('.description').length > 0) {
+      return
+    }
+    if (this.$el.find('.link').has(e.target).length) {
+      return
+    }
+
+    this.toggleFullContent(true)
+  },
+
+  toggleFullContent: function (showFull) {
+    if (showFull) {
+      this.$el.find('.summary').remove()
+      // TODO put some sandboxing restrictions on this
+      var iframe = $('<iframe class="description"></iframe>')
+      var self = this
+
+      iframe.load(function () {
+        var iframeBody = iframe[0].contentDocument.body
+        iframeBody.innerHTML = self.model.get('description')
+
+        // TODO this almost works, but any images loading afterwards will
+        // may change the height. Could have a resizeIframe function that
+        // is called on iframe load AND on each img load
+        iframe.attr('height', iframeBody.scrollHeight + 'px')
+        iframe.attr('width', iframeBody.scrollWidth + 'px')
+
+        self.trigger('ready:iframe')
+      })
+
+      this.$el.append(iframe)
+    } else {
+      this.$el.append('<span class="summary">' + this.model.get('summary') + '</span>')
+      this.$el.find('.description').remove()
+    }
   }
 })
--- client/test/feed-content/article-view-test.js
+++ client/test/feed-content/article-view-test.js
@@ -8,22 +8,82 @@ var Backbone = require('backbone')
 Backbone.$ = require('jQuery')
 
 describe('article-view', function () {
-  var article
+  var article, view
 
   beforeEach(function () {
     article = new Article({
       id: 'id1',
       title: 'title1',
+      summary: 'summary1',
       description: 'description1',
       link: 'http://jerryorr.com/link1'
     })
   })
 
-  it ('renders', function () {
-    var view = new ArticleView({model: article}).render()
-
-    assert.equal(view.$el.find('.title').html(), 'title1')
-    assert.equal(view.$el.find('.description').html(), 'description1')
-    assert.equal(view.$el.find('a.link').attr('href'), 'http://jerryorr.com/link1')
+  afterEach(function () {
+    view && view.remove()
   })
+
+  it ('renders', function () {
+    view = new ArticleView({model: article}).render()
+
+    assertTitle(view, 'title1')
+    assertSummary(view, 'summary1')
+    assertLink(view, 'http://jerryorr.com/link1')
+    assertDescription(view, null)
+  })
+
+  it ('shows full description/content on title click', function (done) {
+    view = new ArticleView({model: article}).render()
+    // Need to have view in DOM so we get iframe load event
+    $('body').append(view.el)
+
+    view.on('ready:iframe', function () {
+      assertTitle(view, 'title1')
+      assertSummary(view, null)
+      assertLink(view, 'http://jerryorr.com/link1')
+      assertDescription(view, 'description1')
+
+      done()
+    })
+
+    view.$el.find('.title').click()
+  })
+
+  function assertTitle(view, html) {
+    if (html) {
+      assert.equal(view.$el.find('.title').html(), html)
+    } else {
+      assert.equal(view.$el.find('.title').length, 0, 'expected no title')
+    }
+  }
+
+  function assertSummary(view, html) {
+    if (html) {
+      assert.equal(view.$el.find('.summary').html(), html)
+    } else {
+      assert.equal(view.$el.find('.summary').length, 0, 'expected no summary')
+    }
+  }
+
+  function assertLink(view, href) {
+    if (href) {
+      assert.equal(view.$el.find('a.link').attr('href'), href)
+    } else {
+      assert.equal(view.$el.find('a.link').length, 0, 'expected no link')
+    }
+  }
+
+  function assertDescription(view, startsWith) {
+    if (startsWith) {
+      var descriptionIframe = view.$el.find('iframe.description')[0]
+      assert(descriptionIframe, 'expected description iframe')
+      var description = descriptionIframe.contentDocument.body.innerHTML
+      assert(description, 'expected description')
+      // assert(description.match(new RegExp('/^' + startsWith)), 'expected description to start with "' + startsWith + '" but was "' + description + '"')
+      assert.match(description, new RegExp('^' + startsWith))
+    } else {
+      assert.equal(view.$el.find('.description').length, 0, 'expected no description')
+    }
+  }
 })
